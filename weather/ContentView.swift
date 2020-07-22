06b9406a457c0e6103d3ccaf386f7c3b3f6458e3
David Rozmajzl <dmrozmajzl@gmail.com>
2020-07-22 16:01:24 -0400

    Initial Commit

---
+++ weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  weather
+//
+//  Created by David Rozmajzl on 7/22/20.
+//  Copyright © 2020 David Rozmajzl. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
